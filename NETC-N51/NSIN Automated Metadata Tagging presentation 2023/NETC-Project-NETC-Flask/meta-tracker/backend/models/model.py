db39b53bd7a425e95c0197be12601ae1d1c9ab5e
Bartholdy Alex <you@example.com>
2023-07-09 21:23:02 -0400

    models placeholder

--- NETC-N51/NSIN Automated Metadata Tagging presentation 2023/NETC-Project-NETC-Flask/meta-tracker/backend/models/model.py
+++ NETC-N51/NSIN Automated Metadata Tagging presentation 2023/NETC-Project-NETC-Flask/meta-tracker/backend/models/model.py
@@ -1,17 +1,16 @@
 #MongoDB databse model to store the files
 from mongoengine import Document, StringField, BinaryField
-
+#Alex models
 class PDFDocument(Document):
     name = StringField()
     data = BinaryField()
 
-
-'''add models for Docs, txt, image, video files'''
-
+#cesar models   --add and edit as needed
 class TxtDocument(Document):
     name = StringField()
     data = BinaryField()
 
+#Garrett models  --add and edit as needs
 class DocDocument(Document):
     name = StringField()
     data = BinaryField()
